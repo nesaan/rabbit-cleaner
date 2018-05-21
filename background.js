d60246c2d7dbe5d57ab4a96efdb73c86d4f8872f
unknown <kszhu@edu.uwaterloo.ca>
2018-05-21 17:56:50 -0400

    Add option to unhide elements once hidden

--- background.js
+++ background.js
@@ -20,11 +20,21 @@ chrome.runtime.onInstalled.addListener(function() {
   });
 });
 
+var cleaned = false;
 chrome.pageAction.onClicked.addListener(function(tab) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-          tabs[0].id,
-          {code: 'document.documentElement.webkitRequestFullscreen();var vetoed = ["social", "toolbar", "barRight", "controls", "banner", "tray"]; for (let i = 0; i < vetoed.length ;i++) {document.getElementsByClassName(vetoed[i])[0].style.display = "none";} document.getElementsByClassName("content")[0].style.maxWidth = "100%";document.body.className = document.body.className.replace("bannerVisible",""); document.getElementById("rabbitapp").classList.remove("toolbarVisible");document.getElementById("rabbitapp").focus();'
-		  });
-    });
+		if (cleaned) {
+			chrome.tabs.executeScript(
+				tabs[0].id,
+				{code: 'var vetoed = ["social", "toolbar", "barRight", "controls", "banner", "tray"]; for (let i = 0; i < vetoed.length ;i++) {document.getElementsByClassName(vetoed[i])[0].style.display = null;} document.getElementById("rabbitapp").className += " toolbarVisible";document.getElementById("rabbitapp").focus();'
+			});
+		}
+		else {
+			chrome.tabs.executeScript(
+				tabs[0].id,
+				{code: 'document.documentElement.webkitRequestFullscreen();var vetoed = ["social", "toolbar", "barRight", "controls", "banner", "tray"]; for (let i = 0; i < vetoed.length ;i++) {document.getElementsByClassName(vetoed[i])[0].style.display = "none";} document.getElementsByClassName("content")[0].style.maxWidth = "100%";document.body.className = document.body.className.replace("bannerVisible",""); document.getElementById("rabbitapp").classList.remove("toolbarVisible");document.getElementById("rabbitapp").focus();'
+			});
+		}
+		cleaned = !cleaned;
+	});
 });
