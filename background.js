4d53b5b3e1309a6495f590340b3dd5fd92ac454f
unknown <kszhu@edu.uwaterloo.ca>
2018-06-30 03:37:17 -0400

    v1.2: Fixed top bar on hover and persisting variables

--- background.js
+++ background.js
@@ -23,18 +23,19 @@ chrome.runtime.onInstalled.addListener(function() {
 var cleaned = false;
 chrome.pageAction.onClicked.addListener(function(tab) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		if (cleaned) {
+		//if (cleaned) {
 			chrome.tabs.executeScript(
 				tabs[0].id,
-				{code: 'var vetoed = ["social", "toolbar", "barRight", "controls", "banner", "tray"]; for (let i = 0; i < vetoed.length ;i++) {document.getElementsByClassName(vetoed[i])[0].style.display = null;} document.getElementById("rabbitapp").className += " toolbarVisible";document.getElementById("rabbitapp").focus();'
+				{code: 'var vetoed = ["social", "toolbar", "barRight", "controls", "banner", "tray"]; if (document.getElementsByClassName(vetoed[0])[0].style.display == "none") { for (let i = 0; i < vetoed.length ;i++) {document.getElementsByClassName(vetoed[i])[0].style.display = null;} document.getElementById("rabbitapp").className += " toolbarVisible";document.getElementById("rabbitapp").focus(); } else {  document.documentElement.webkitRequestFullscreen(); for (let i = 0; i < vetoed.length ;i++) {document.getElementsByClassName(vetoed[i])[0].style.display = "none";} document.getElementsByClassName("content")[0].style.marginTop = "0"; document.getElementsByClassName("content")[0].style.maxWidth = "100%";document.body.className = document.body.className.replace("bannerVisible",""); document.getElementById("rabbitapp").classList.remove("toolbarVisible");document.getElementById("rabbitapp").focus(); } '
+
 			});
-		}
+		/*}
 		else {
 			chrome.tabs.executeScript(
 				tabs[0].id,
 				{code: 'document.documentElement.webkitRequestFullscreen();var vetoed = ["social", "toolbar", "barRight", "controls", "banner", "tray"]; for (let i = 0; i < vetoed.length ;i++) {document.getElementsByClassName(vetoed[i])[0].style.display = "none";} document.getElementsByClassName("content")[0].style.maxWidth = "100%";document.body.className = document.body.className.replace("bannerVisible",""); document.getElementById("rabbitapp").classList.remove("toolbarVisible");document.getElementById("rabbitapp").focus();'
 			});
 		}
-		cleaned = !cleaned;
+		cleaned = !cleaned;*/
 	});
 });
